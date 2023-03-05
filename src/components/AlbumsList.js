d16d68c0e6c9bfd39f6676f63516e0913d99894e
Rachel Spencer <rachelspencer688@gmail.com>
2023-03-05 13:13:03 -0500

    show album working

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,5 +1,32 @@
+import { useFetchAlbumsQuery } from "../store";
+import Skeleton from './Skeleton';
+import ExpandablePanel from './ExpandablePanel';
+import Button from './Button';
+
 function AlbumsList({ user }) {
-  return <div>Albums for {user.name}</div>;
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+
+  let content;
+  if (isLoading) {
+    content = <Skeleton time={3} />
+  } else if (error) {
+    content = <div>Error Loading albums.</div>;
+  } else {
+    content = data.map(album => {
+      const header = <div>{album.title}</div>;
+
+      return <ExpandablePanel key={album.id} header={header}>
+        List of Photos in the album
+      </ExpandablePanel>
+    });
+  }
+
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;

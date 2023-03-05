25007022d56f620471472c604dfac9f7f70f411d
Rachel Spencer <rachelspencer688@gmail.com>
2023-03-05 16:13:27 -0500

    tag validation complete

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,10 +1,16 @@
-import { useFetchAlbumsQuery } from "../store";
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
 import Skeleton from './Skeleton';
 import ExpandablePanel from './ExpandablePanel';
 import Button from './Button';
 
 function AlbumsList({ user }) {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  useFetchAlbumsQuery(user)
+  const [addAlbum, results] = useAddAlbumMutation();
+
+  const handleAddAlbum = () => {
+    addAlbum(user);
+  };
 
   let content;
   if (isLoading) {
@@ -23,7 +29,11 @@ function AlbumsList({ user }) {
 
   return (
     <div>
-      <div>Albums for {user.name}</div>
+      <div>
+        Albums for {user.name}
+        <Button onClick={handleAddAlbum}>
+          + Add Album</Button>
+      </div>
       <div>{content}</div>
     </div>
   );

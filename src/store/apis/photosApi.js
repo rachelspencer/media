8ff61159b616dbb6f9beed7a3f1e4aadfe186847
Rachel Spencer <rachelspencer688@gmail.com>
2023-03-05 19:39:12 -0500

    photo api created

---
+++ src/store/apis/photosApi.js
@@ -0,0 +1,51 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { faker } from '@faker-js/faker';
+
+const photosApi = createApi({
+    reducerPath: 'photos',
+    baseQuery: fetchBaseQuery({
+        baseUrl:'http://localhost:3005',
+    }),
+    endpoints(builder){
+        return {
+            fetchPhotos: builder.query({
+                query: (album) => {
+                    return {
+                        url: '/photos',
+                        method: 'GET',
+                        params: {
+                            albumId: album.id
+                        },
+                    };
+                },
+            }),
+            addPhoto: builder.mutation({
+                query: (album) => {
+                    return {
+                        url: '/photos',
+                        method: 'POST',
+                        params: {
+                            albumID: album.id
+                        },
+                        body: faker.image.abstract(150, 150, true)
+                    };
+                },
+            }),
+            removePhoto: builder.mutation({
+                query: (photo) => {
+                    return {
+                        url: `/photos/${photo.id}`,
+                        method: 'DELETE',
+                    };
+                },
+            }),
+        };
+    },
+});
+
+export const { 
+    useFetchPhotosQuery, 
+    useAddPhotosMutation, 
+    useRemovePhotosMutation } 
+= photosApi;
+export { photosApi };
